a36203f2468a19125021c04ad3d2e2a3507f6087
nmiwasaki <noahiwasaki04@gmail.com>
2024-07-21 11:07:32 -0400

    2015 Day 2 Part 1 Complete

--- 2015/Day 2/2-1.py
+++ 2015/Day 2/2-1.py
@@ -1,5 +1,19 @@
-import os
-# print(os.getcwd())
+import re
+pattern = r"(\d+)x(\d+)x(\d+)"
 
 with open("2015/Day 2/input.txt") as f:
-    filecontents = f.read()
+    filecontents = f.readlines()
+
+    paper_needed: int = 0
+
+    for line in filecontents:
+        match = re.match(pattern, line)
+        if match:
+            dimensions = [int(length) for length in match.groups()]
+            paper_size = 2 * (dimensions[0] * dimensions[1] + dimensions[1] * dimensions[2] + dimensions[2] * dimensions[0]) + \
+                         min(dimensions[0] * dimensions[1], dimensions[1] * dimensions[2], dimensions[2] * dimensions[0])
+            paper_needed += paper_size
+        else:
+            print("ERROR")
+            break
+    print(f"The required area of paper is {paper_needed}")
